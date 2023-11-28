90a1a6f5831b0bf5577e11ef432b27b83747cc2d
vvl10p <vvl10p@SZA0030620>
2023-11-28 10:46:35 +0100

    Add project files.

---
+++ hands-on-lab/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using hands_on_lab.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hands_on_lab.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult osszesKerdes()
+        {
+            HajosContext context = new HajosContext();
+            var kerdesek = from x in context.Questions select x.Question1;
+
+            return Ok(kerdesek);
+        }
+    }
+}
---
+++ hands-on-lab/Controllers/TesztController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hands_on_lab.Controllers
+{
+    //Commentbe vele!
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/pontosido")]
+        public IActionResult pontosIdo()
+        {
+            string pontosido = DateTime.Now.ToShortTimeString();
+
+            return Ok(pontosido);
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+
+        public IActionResult nagyBetus(string szoveg)
+        {
+            return Ok(szoveg.ToUpper());
+        }
+    }
+}
